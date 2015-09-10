c9bbcc22f9fd8918f28a34ea9350a64c002a8493
Trevor Toryk <TrevT93@gmail.com>
2015-09-10 09:32:20 -0400

    First changes to the service (transferring from one machine to another).

--- dsservice/service.py
+++ dsservice/service.py
@@ -8,6 +8,21 @@ from spyne.model.complex import Iterable, Array
 from spyne.model.fault import Fault
 
 from dsservice.types import DutyFilter, DutyInfo, DutyAllStrategies
+from dsservice.database import Duty, DutyCode, DutyStrategy, makeSession
+
+from hashlib import sha256
+
+_duties = {}	# A dict with the prime key as the DutyCode.
+				# The values of these keys are dicts that
+				# map to the many values of the duty.  When
+				# using AddDuty or any of the Set methods,
+				# these entries must be updated as well as
+				# the SQLite DB.
+
+_dutyStrategies = {}	# A dict with the prime key as the DutyCode.
+						# The values of these keys are dicts that
+						# map to the four roles ('dps', 'tank',
+						# 'healer' and 'misc') and their strategies.
 
 _version = "0.0.1"
 
@@ -18,12 +33,16 @@ class DSService(ServiceBase):
 		return "FFX|V Duty Strategy Service, Version %s" % _version
 
 
+##########
+
 
 	@rpc(Array(DutyFilter), _returns = Iterable(Unicode))
 	def FilterDutyCodes(request, dutyFilters):
 		"""Filters the database of duties based on the given parameters.  The filtering is actually quite complex and will be documented in a separate...docstring.  You can pass it a blank array to just send everything back."""
 		raise Fault("Not implemented.")
 
+
+##########
 
 
 	@rpc(Unicode, _returns = DutyInfo)
@@ -32,12 +51,16 @@ class DSService(ServiceBase):
 		raise Fault("Not implemented.")
 
 
+##########
+
 
 	@rpc(Unicode, Unicode, _returns = Unicode)
 	def GetDutyStrategy(request, dutyCode, role):
 		"""Gets the strategy for a single duty and role.  Throws an error if the dutyCode or role is not found.  Possible roles: "dps", "healer", "tank", "misc" (where misc returns data universal to all three jobs)."""
 		raise Fault("Not implemented.")
 
+
+##########
 
 
 	@rpc(Unicode, _returns = DutyAllStrategies)
@@ -46,12 +69,55 @@ class DSService(ServiceBase):
 		raise Fault("Not implemented.")
 
 
+##########
+
 
 	@rpc(Unicode, Unicode, Unicode, _returns = Unicode)
 	def AddDuty(request, magicHash, dutyCode, dutyType):
 		"""If you know the magicHash, allows you to add a duty to the database."""
-		raise Fault("Not implemented.")
+		# Verification of the data
+		if len(dutyCode) > 16:
+			raise Fault(faultcode = "Client.DutyCode", faultstring = "Code is too long (must be <16 chars).")
+		
+		if not (dutyType == "dungeon" or dutyType == "trial" or dutyType == "raid" or dutyType == "guildhest" or dutyType == "roulette":
+			raise Fault(faultcode = "Client.DutyType", faultstring = "Only the following values are allowed: 'dungeon', 'trial', 'raid', 'guildhest', 'roulette'.")
+		
+		# Verification of the hash
+		if sha256(magicHash).hexdigest() == 'f43e52381d33c58a4362ebc646a4924b81448e4383dff916ebce8b4c1a4627bc':
+			# Try to connect to the database
+			try:
+				sesh = makeSession()
+			except:
+				raise Fault(faultcode = "Server.SessionError", faultstring = "Database session creation failed.")
+			
+			# Try to add the duty
+			try:
+				newDuty = DutyCode(id = dutyCode, type = dutyType)
+				sesh.add(newDuty)
+			except:
+				sesh.close()
+				raise Fault(faultcode = "Server.DataError", faultstring = "Database was unable to create the new duty.")
+			
+			# Try to commit the data
+			try:
+				sesh.commit()
+			except:
+				sesh.rollback()
+				sesh.close()
+				raise Fault(faultcode = "Server.CommitError", faultstring = "Database was unable to commit the new duty.")
+			
+			# Add the duty to the master dict
+			global _duties, _dutyStrategies
+			_duties[dutyCode] = {}
+			_dutyStrategies[dutyCode] = {}
+			
+			sesh.close()
+			return "OK"
+		else:
+			raise Fault(faultcode = "Client.BadHash", faultstring = "Access denied due to incorrect magicHash.")
 
+
+##########
 
 
 	@rpc(Unicode, Unicode, Unicode, Unicode, _returns = Unicode)
@@ -59,6 +125,8 @@ class DSService(ServiceBase):
 		"""If you know the magicHash, allows you to set a duty's property (any of the miscellaneous fields, not strategy information)."""
 		raise Fault("Not implemented.")
 
+
+##########
 
 
 	@rpc(Unicode, Unicode, Unicode, Unicode, _returns = Unicode)
