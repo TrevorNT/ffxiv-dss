3fda55158cd6d6a48d0df6894eee7456b29b290e
Trevor Toryk <TrevT93@gmail.com>
2015-09-14 16:50:37 -0400

    Removed the DutyCode table; it wasn't necessary anyway.  Foreign keys updated.  Also, "tomestonesAwarded" changed to "tomestonesRewarded" for consistency.

--- dsservice/database.py
+++ dsservice/database.py
@@ -5,22 +5,24 @@ from sqlalchemy.orm import sessionmaker
 _engine = create_engine('sqlite:///dss.sqlite3.db')
 _tableBase = declarative_base()
 
-class DutyCode(_tableBase):
-	__tablename__ = 'dutycodes'
-	
-	id = Column(String(16), primary_key = True)
-	dutyType = Column(String(16), nullable = False)
-	
-	def __repr__(self):
-		return "<DutyCode(id='%s', dutyType='%s')>" % (self.id, self.dutyType)
-	
-	def __str__(self):
-		return "<DutyCode: %s>" % self.id
+#class DutyCode(_tableBase):
+#	__tablename__ = 'dutycodes'
+#	
+#	id = Column(String(16), primary_key = True)
+#	dutyType = Column(String(16), nullable = False)
+#	
+#	def __repr__(self):
+#		return "<DutyCode(id='%s', dutyType='%s')>" % (self.id, self.dutyType)
+#	
+#	def __str__(self):
+#		return "<DutyCode: %s>" % self.id
 
 class Duty(_tableBase):
 	__tablename__ = 'duties'
 
-	id = Column(String(16), ForeignKey("dutycodes.id"), primary_key = True)
+#	id = Column(String(16), ForeignKey("dutycodes.id"), primary_key = True)
+	id = Column(String(16), primary_key = True)
+	type = Column(String(16), nullable = False)
 	name = Column(String)
 	description = Column(String)
 	imageLocation = Column(String)
@@ -33,13 +35,13 @@ class Duty(_tableBase):
 	iLevelSync = Column(Integer)
 	roulette = Column(String)
 	tomestoneType = Column(String)
-	tomestonesAwarded = Column(Integer)
+	tomestonesRewarded = Column(Integer)
 	xpRewarded = Column(Integer)
 	gilRewarded = Column(Integer)
 	partySize = Column(Integer)
 	
 	def __repr__(self):
-		return "<Duty(id='%s', name='%s', description='%s', imageLocation='%s', receivedFromQuest='%s', isMainStory='%s', expansion=%d, levelMin=%d, levelMax=%d, iLevelMin=%d, iLevelSync=%d, roulette='%s', tomestoneType='%s', tomestonesAwarded=%d, xpRewarded=%d, gilRewarded=%d, partySize=%d)>" % (self.id, self.name, self.description, self.imageLocation, self.receivedFromQuest, str(self.isMainStory), self.expansion, self.levelMin, self.levelMax, self.iLevelMin, self.iLevelSync, self.roulette, self.tomestoneType, self.tomestonesAwarded, self.xpRewarded, self.gilRewarded, self.partySize)
+		return "<Duty(id='%s', name='%s', description='%s', imageLocation='%s', receivedFromQuest='%s', isMainStory='%s', expansion=%d, levelMin=%d, levelMax=%d, iLevelMin=%d, iLevelSync=%d, roulette='%s', tomestoneType='%s', tomestonesRewarded=%d, xpRewarded=%d, gilRewarded=%d, partySize=%d)>" % (self.id, self.name, self.description, self.imageLocation, self.receivedFromQuest, str(self.isMainStory), self.expansion, self.levelMin, self.levelMax, self.iLevelMin, self.iLevelSync, self.roulette, self.tomestoneType, self.tomestonesRewarded, self.xpRewarded, self.gilRewarded, self.partySize)
 	
 	def __str__(self):
 		return "<Duty Name: %s, ID: %s>" % (self.name, self.id)
@@ -49,7 +51,8 @@ class Duty(_tableBase):
 class DutyStrategy(_tableBase):
 	__tablename__ = 'duty_strategies'
 	
-	dutyId = Column(String(16), ForeignKey("dutycodes.id"), primary_key = True)
+#	dutyId = Column(String(16), ForeignKey("dutycodes.id"), primary_key = True)
+	dutyId = Column(String(16), ForeignKey("duties.id"), primary_key = True)
 	role = Column(String(16), primary_key = True)
 	strategy = Column(String)
 	
